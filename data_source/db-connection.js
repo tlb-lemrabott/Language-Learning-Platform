6a5d75fb01cc500680f6f52f1fc94945a3b4466d
Lemrabott <llemrabott52@gmail.com>
2023-06-15 17:18:58 -0500

    updating dbConnection methods to work with  promises

--- data_source/db-connection.js
+++ data_source/db-connection.js
@@ -1,17 +1,7 @@
 const mongoose = require("mongoose");
 require('./models/language');
-require('dotenv').config();
 
-mongoose.connect(process.env.CONNEXION_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connection.on(process.env.CONNECTED_EVENT, function(){
-    console.log(process.env.CONNECTION_MSG_INFO, process.env.DB_NAME);
-});
-
-mongoose.connection.on(process.env.DISCONNECTED_EVENT, function(){
-    console.log(process.env.DISCONNECTION_MSG_INFO, process.env.DB_NAME);
-});
-
-mongoose.connection.on(process.env.ERROR_EVENT, function(err){
-    console.log(process.env.CNX_ERROR_MSG,err);
-});
+mongoose.connect(process.env.DB_URL)
+    .then(() => console.log(process.env.CONNECTION_MSG_INFO))
+    .catch((err) => console.log(process.env.CNX_ERROR_MSG, err));
