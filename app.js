4f8feecfa9845fe4688eccd8ba3eb7d233b5b723
Lemrabott <llemrabott52@gmail.com>
2023-06-15 17:19:51 -0500

    updating app.js: adding cors for http requests and methods

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 require('dotenv').config();
 require('./data_source/db-connection');
-const routes = require('./routes/languageRouter');
+const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+// app.use("/api", function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+//     next();
+// });
+
+// app.use(cors({
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }));
 
 
 app.use('/api', routes);
@@ -13,5 +24,5 @@ app.use('/api', routes);
 
 app.listen(process.env.PORT, function(){
     const port = process.env.PORT
-    console.log(process.env.SERVER_INFORMATION+port);
+    console.log(process.env.MSG_SERVER_START+port);
 });
