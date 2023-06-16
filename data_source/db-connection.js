e6f5b48079603ecfd1029cb17ee23aae1794d13f
Lemrabott <llemrabott52@gmail.com>
2023-06-16 01:08:25 -0500

    update db and connection package name source

--- data_source/db-connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-require('./models/language');
-
-
-mongoose.connect(process.env.DB_URL)
-    .then(() => console.log(process.env.CONNECTION_MSG_INFO))
-    .catch((err) => console.log(process.env.CNX_ERROR_MSG, err));
