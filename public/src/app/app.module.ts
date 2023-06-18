f7986896274aa19e3795bacd2784fb8c3b0e2293
Lemrabott <llemrabott52@gmail.com>
2023-06-18 11:31:12 -0500

    update app module import new modules

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,22 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { LanguageComponent } from './language/language.component';
+
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
+import { AuthentificationInterceptor } from './authentification.interceptor';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    LanguagesComponent,
+    LanguageComponent,
+    RegisterComponent,
+    LoginComponent,
+    BooksComponent,
+    BookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthentificationInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
