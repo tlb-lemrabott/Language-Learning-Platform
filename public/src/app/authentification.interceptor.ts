3ecb46290b25f2dc8ca74c504b2b71d306ed513f
Lemrabott <llemrabott52@gmail.com>
2023-06-18 11:37:59 -0500

    add authentication interceptor

---
+++ public/src/app/authentification.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable()
+export class AuthentificationInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthenticationService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    
+    const token = this.authService.getToken();
+
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  
+  }
+
+
+
+}
